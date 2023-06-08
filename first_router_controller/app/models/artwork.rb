ab56e10a3eb22bc08f444507b8eb1094f016603e
Juan A <avilajajuan@gmail.com>
2023-06-08 16:12:09 -0400

    associations

--- first_router_controller/app/models/artwork.rb
+++ first_router_controller/app/models/artwork.rb
@@ -1,6 +1,31 @@
+# == Schema Information
+#
+# Table name: artworks
+#
+#  id         :bigint           not null, primary key
+#  title      :string           not null
+#  image_url  :string           not null
+#  artist_id  :bigint           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class Artwork < ApplicationRecord
     validates :title, presence: true
     validates :image_url, presence: true, uniqueness: true
     validates :artist_id, presence: true
     validates :title, uniqueness: {scope: :artist_id}   
+
+
+    belongs_to :artist,
+        foreign_key: :artist_id,
+        class_name: :User
+        
+    has_many :viewers,
+    foreign_key: :viewer_id,
+    class_name: :User
+
+    
+
+    
+    
 end
--- first_router_controller/app/models/user.rb
+++ first_router_controller/app/models/user.rb
@@ -1,3 +1,25 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id         :bigint           not null, primary key
+#  username   :string           not null
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 class User < ApplicationRecord
     validates :username, presence: true, uniqueness: true
+
+    has_many :viewers,
+    foreign_key: :viewer_id,
+    class_name: :User
+
+    has_many :artworks, ##???????
+        foreign_key: :artwork_id,
+        class_name: :ArtworkShare
+
+    has_many :shared_artworks,
+        through: :viewers,
+        source: :artwork
+
 end
